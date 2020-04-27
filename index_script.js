2cae234ec3a45cad3f0eac69d51ae6f70ff72257
Soumitra Mandal <soumitramandal1999@gmail.com>
2020-04-27 14:41:51 +0530

    huge Update

---
+++ index_script.js
@@ -0,0 +1,13 @@
+function weatherfunc(){
+var city= document.getElementById("C").value;
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=70207a5efd2148a93db198be86385483",function(data){
+console.log(data);
+var icon="http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+var temp = data.main.temp;
+var weather = data.weather[0].main;
+document.getElementById("weather").innerHTML=weather;
+document.getElementById("cityn").innerHTML=city;
+document.getElementById("temp").innerHTML=temp + "°C";
+$('.icon').attr('src', icon);
+});
+}
