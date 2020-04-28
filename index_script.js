f5d1a590b77f8ec70449c1b12d6cf3cf5fcc28f8
Soumitra Mandal <soumitramandal1999@gmail.com>
2020-04-28 10:16:30 +0530

    https update

--- index_script.js
+++ index_script.js
@@ -1,8 +1,8 @@
 function weatherfunc(){
 var city= document.getElementById("C").value;
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=70207a5efd2148a93db198be86385483",function(data){
+$.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=70207a5efd2148a93db198be86385483",function(data){
 console.log(data);
-var icon="http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+var icon="https://crossorigin.me/http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 var temp = data.main.temp;
 var weather = data.weather[0].main;
 document.getElementById("weather").innerHTML=weather;
