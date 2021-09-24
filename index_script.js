39e33694daa3621666cb7419d065ca42cf79dfa8
Soumitra Mandal <soumitramandal1999@gmail.com>
2021-09-24 15:39:31 +0530

    API key Renewed

--- index_script.js
+++ index_script.js
@@ -1,7 +1,7 @@
 
 function weatherfunc(){
 var city= document.getElementById("C").value;
-$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=70207a5efd2148a93db198be86385483",function(data){
+$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=0290ed9eafcde113b27e2859a7f63fb1",function(data){
 console.log(data);
 var temp = data.main.temp;
 var weather = data.weather[0].main;
