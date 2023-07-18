d64f0270383535fc4630f010cf3976bb1ed4191a
Soumitra Mandal <soumitramandal1999@gmail.com>
2023-07-18 14:21:02 +0530

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -1,8 +1,8 @@
 
 function weatherfunc(){
 var city= document.getElementById("C").value;
-$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=f88972d83df149fc5c21ed06a1757053",function(data){
-console.log(data);
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=f88972d83df149fc5c21ed06a1757053",function(data, status, xhr){
+xhr.withCredentials = true;
 var temp = data.main.temp;
 var weather = data.weather[0].main;
 document.getElementById("weather").innerHTML=weather;
